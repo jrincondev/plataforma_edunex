e2ff2400e2c962937a294e3fd9d585082d5a3586
sergioeve27 <ramirezbenitez2244@gmail.com>
2025-06-19 22:29:31 -0500

    creer las tablas, portafolio_instructor, tipo_evidencia, trimestre_instructor

---
+++ app/Http/Controllers/PortafolioInstructorController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Portafolio_instructor;
+use Illuminate\Http\Request;
+
+class PortafolioInstructorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Portafolio_instructor $portafolio_instructor)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Portafolio_instructor $portafolio_instructor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Portafolio_instructor $portafolio_instructor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Portafolio_instructor $portafolio_instructor)
+    {
+        //
+    }
+}
